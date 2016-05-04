503eabb650912df6097efd28074fb64b1731d586
黄健达 <lsr@sina.com>
2016-05-04 23:46:16 +0800

    p24

---
+++ src/main/java/highlight_spring4/ch1/javaconfig/FunctionService.java
@@ -0,0 +1,9 @@
+package highlight_spring4.ch1.javaconfig;
+
+public class FunctionService {
+
+	public String sayHello(String world) {
+		return "Hello "+world+" !";
+	}
+
+}
---
+++ src/main/java/highlight_spring4/ch1/javaconfig/UseFunctionService.java
@@ -0,0 +1,18 @@
+package highlight_spring4.ch1.javaconfig;
+
+public class UseFunctionService {
+	public FunctionService getFunctionService() {
+		return functionService;
+	}
+
+	public void setFunctionService(FunctionService functionService) {
+		this.functionService = functionService;
+	}
+
+	FunctionService functionService;
+
+	public String sayHello(String word) {
+		return functionService.sayHello(word);
+	}
+
+}
