14086f38ae9c250ef3a805affcabbd3ab43c81d8
黄健达 <lsr@sina.com>
2016-05-05 23:58:55 +0800

    ch2

---
+++ src/main/java/highlight_spring4/ch2/scope/DemoSingleonService.java
@@ -0,0 +1,11 @@
+package highlight_spring4.ch2.scope;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoSingleonService {
+
+	public DemoSingleonService() {
+		// TODO Auto-generated constructor stub
+	}
+}
