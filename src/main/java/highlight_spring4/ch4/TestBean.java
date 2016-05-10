78590bc762703699593f24b5767ad2f31f26afbe
黄健达 <lsr@sina.com>
2016-05-10 23:27:40 +0800

    20160510

---
+++ src/main/java/highlight_spring4/ch4/TestBean.java
@@ -0,0 +1,19 @@
+package highlight_spring4.ch4;
+
+public class TestBean {
+
+	private String content;
+
+	public TestBean(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
